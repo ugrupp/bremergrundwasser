710b5463dea1c1412ee01d82c49f024ebfcd2136
Urs Grupp <urs@21sieben.de>
2022-10-12 17:46:41 +0200

    fix: improve hero loading behaviour

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,17 +1,23 @@
 import classNames from "classnames";
 import NextImage from "next/image";
-import { HTMLAttributes } from "react";
+import { HTMLAttributes, useState } from "react";
 import data from "../data/bewaesserung.json";
 import { NEXT_IMAGE_DEFAULT_QUALITY } from "../lib/constants";
 import styles from "../styles/hero.module.css";
 import Container from "./container";
-import Image from "./image";
 
 interface HeroSectionProps extends HTMLAttributes<HTMLElement> {
   data: typeof data.hero;
 }
 
 const HeroSection = ({ data, className }: HeroSectionProps): JSX.Element => {
+  const [isImageLoadingComplete, setIsImageLoadingComplete] =
+    useState<boolean>(false);
+  const [isSchwungLoadingComplete, setIsSchwungLoadingComplete] =
+    useState<boolean>(false);
+
+  const imagesLoaded = isImageLoadingComplete && isSchwungLoadingComplete;
+
   return (
     <section
       className={classNames(
@@ -29,20 +35,38 @@ const HeroSection = ({ data, className }: HeroSectionProps): JSX.Element => {
         >
           <div className="h-[115%] md:h-[150%] w-[115%] shrink-0 flex items-center justify-center">
             <div className="aspect-square min-w-full min-h-full relative shrink-0 rounded-full overflow-hidden">
-              <Image
-                wrapperProps={{
-                  className: "w-full h-full rounded-full overflow-hidden",
-                }}
-                quality={NEXT_IMAGE_DEFAULT_QUALITY}
-                layout="fill"
-                src={data.image.src}
-                alt={data.image.alt}
-                objectFit="cover"
-                objectPosition={data.image.objectPosition}
-                dominantColor={data.image.dominantColor}
-              />
+              <div className="!absolute inset-0 bg-brown-700/5"></div>
 
-              <div className="!absolute inset-0">
+              <div
+                className={classNames(
+                  "relative w-full h-full rounded-full overflow-hidden z-10 transition-opacity duration-500",
+                  {
+                    "opacity-0": !imagesLoaded,
+                  }
+                )}
+              >
+                <NextImage
+                  quality={NEXT_IMAGE_DEFAULT_QUALITY}
+                  layout="fill"
+                  src={data.image.src}
+                  alt={data.image.alt}
+                  objectFit="cover"
+                  objectPosition={data.image.objectPosition}
+                  priority
+                  onLoadingComplete={() => {
+                    setIsImageLoadingComplete(true);
+                  }}
+                />
+              </div>
+
+              <div
+                className={classNames(
+                  "!absolute inset-0 z-20 transition-opacity duration-500",
+                  {
+                    "opacity-0": !imagesLoaded,
+                  }
+                )}
+              >
                 <NextImage
                   quality={NEXT_IMAGE_DEFAULT_QUALITY}
                   layout="fill"
@@ -50,6 +74,10 @@ const HeroSection = ({ data, className }: HeroSectionProps): JSX.Element => {
                   alt=""
                   objectFit="contain"
                   objectPosition={"100% 100%"}
+                  priority
+                  onLoadingComplete={() => {
+                    setIsSchwungLoadingComplete(true);
+                  }}
                 />
               </div>
             </div>
