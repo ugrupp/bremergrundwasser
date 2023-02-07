87d06386dab7d77866262e4f6d82453603f3c9ab
Urs Grupp <urs@21sieben.de>
2023-02-07 01:32:56 +0100

    fix: fix spacing bug

--- src/components/verkauf/pumps.tsx
+++ src/components/verkauf/pumps.tsx
@@ -35,7 +35,7 @@ const PumpsSection = ({ data, className }: PumpsSectionProps): JSX.Element => {
           </div>
 
           {/* Pumps */}
-          <div className="col-start-2 col-end-[-2] md:col-start-3 md:col-end-[-3] lg:col-start-1 lg:col-end-[-1] lg:row-start-2">
+          <div className="col-start-2 col-end-[-2] md:col-start-3 md:col-end-[-3] lg:col-start-1 lg:col-end-[-1]">
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-30 md:gap-60">
               {resolvePumps(data.pumps).map((pump) => (
                 <Pump key={pump.id} pump={pump} />
@@ -44,7 +44,7 @@ const PumpsSection = ({ data, className }: PumpsSectionProps): JSX.Element => {
           </div>
 
           {/* Hint */}
-          <div className="col-start-2 col-end-[-2] md:col-start-3 md:col-end-[-3] lg:col-start-1 lg:col-end-9 lg:row-start-2 lg:self-end">
+          <div className="col-start-2 col-end-[-2] md:col-start-3 md:col-end-[-3] lg:col-start-1 lg:col-end-9">
             <div
               className={classNames(
                 richtextStyles.root,
