3124428fc2cd06a1ad9a648663270534535f920a
Mirko Poloni <mirkopoloni@gmail.com>
2023-04-07 15:40:47 +0200

    content: add gallery brunnen

--- src/pages/brunnen.tsx
+++ src/pages/brunnen.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import BrunnenSection from "../components/brunnen/brunnen";
 import ContactSection from "../components/brunnen/contact";
 import PumpsSection from "../components/brunnen/pumps";
+import Carousel from "../components/carousel";
 import HeroSection from "../components/hero";
 import data from "../data/brunnen.json";
 import staticData from "../data/static.json";
@@ -34,6 +35,7 @@ const Brunnen: NextPage<PageProps> = ({ data }: PageProps) => {
           data={data.pumps}
           className="mt-80 lg:mt-120 pb-80 lg:pb-120"
         />
+        <Carousel carousel={data.carousel.carousel} />
       </div>
     </>
   );
