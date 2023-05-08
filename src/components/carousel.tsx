3b8965933a2e720b9c3b4fb188cebd036006e46b
Mirko Poloni <32099118+mirkopoloni@users.noreply.github.com>
2023-05-08 17:57:59 +0200

    feat: add navigation buttons to carousel on desktop

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,11 +1,12 @@
 import classNames from "classnames";
 import type { HTMLAttributes } from "react";
 import { useState } from "react";
-import { Pagination, Autoplay } from "swiper";
+import { Pagination, Autoplay, Navigation } from "swiper";
 import "swiper/css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { NEXT_IMAGE_DEFAULT_QUALITY } from "../lib/constants";
 import styles from "../styles/carousel.module.css";
+import ArrowIcon from "../assets/icons/arrow-1.svg";
 import { Image as ImageType } from "../types/image";
 import Container from "./container";
 import Image from "./image";
@@ -22,11 +23,13 @@ export const Carousel = ({
   ...props
 }: CarouselProps): JSX.Element => {
   const [paginationEl, setPaginationEl] = useState<HTMLDivElement | null>(null);
+  const [navigationPrev, setNavigationPrev] = useState<HTMLButtonElement | null>(null);
+  const [navigationNext, setNavigationNext] = useState<HTMLButtonElement | null>(null);
 
   return (
     <div className={classNames(className, styles.root, "relative")} {...props}>
       <Swiper
-        modules={[Pagination, Autoplay]}
+        modules={[Navigation, Pagination, Autoplay]}
         loop={true}
         autoplay={true}
         slidesPerView={1}
@@ -38,7 +41,11 @@ export const Carousel = ({
         preloadImages={false}
         pagination={{
           el: paginationEl,
-          type: "bullets",
+          clickable: true,
+        }}
+        navigation={{
+          prevEl: navigationPrev,
+          nextEl: navigationNext,
         }}
       >
         {carousel.map(({ image }, idx) => (
@@ -60,10 +67,21 @@ export const Carousel = ({
       </Swiper>
 
       {/* Controls */}
-      <div className="absolute inset-x-0 bottom-20 md:bottom-40 z-10 pointer-events-none">
+      <div className="absolute inset-x-0 bottom-20 md:bottom-40 z-10 pointer-events-none lg:pointer-events-auto lg:cursor-pointer">
         <Container>
           {/* Pagination */}
           <div ref={(node) => setPaginationEl(node)} />
+        </Container>
+      </div>
+      {/* Navigation */}
+      <div className="hidden lg:block absolute inset-x-0 bottom-75 z-10">
+        <Container classNameInner="flex space-x-20">
+          <button ref={(node) => setNavigationPrev(node)}>
+            <ArrowIcon className="rotate-180 text-white hover:text-teal-300 h-30 w-30" />
+          </button>
+          <button ref={(node) => setNavigationNext(node)}>
+            <ArrowIcon className="text-white hover:text-teal-300 h-30 w-30" />
+          </button>
         </Container>
       </div>
     </div>
